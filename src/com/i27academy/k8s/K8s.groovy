42ff2b6e5c5fbe9ae6c47d27ff692aa24a3b1391
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-12-24 23:17:56 +0530

    dd

---
+++ src/com/i27academy/k8s/K8s.groovy
@@ -0,0 +1,13 @@
+package com.i27academy.k8s
+
+class K8s {
+    def jenkins
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+
+   //we will write all the kubernets related files here 
+
+
+
+}
