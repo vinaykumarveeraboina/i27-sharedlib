c191cd60d7f732326814ef4d7268366d644c82f5
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-12-25 11:52:40 +0530

    dd

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -8,6 +8,16 @@ class K8s {
 
    //we will write all the kubernets related files here 
 
+      def akslogin()
+      
+     {
+                    jenkins.sh """
+                    
+                            az login --service-principal -u e174eda7-af95-4e7b-a3dc-c7b307f19ac5 -p WVQ8Q~PwIQ_R.Lt8dJSFN5iS2XacHPTwBIVnkaMP --tenant cf10d2ad-2334-4fc8-81da-b44a26739199
+                            az account set --subscription b056fb7b-25fb-4735-9bd4-39d1c66b6e5a
 
 
+                        """
+
+}
 }
