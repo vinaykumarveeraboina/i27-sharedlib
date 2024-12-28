14ec34fdbdb936387eea13d6a96be77cce8b7982
vinay veeraboina <vinaykumarveeraboina@gmail.com>
2024-12-28 13:38:59 +0530

    ded

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -78,7 +78,7 @@ class K8s {
 
         echo "checking if helm chat exists"
 
-        if helm list | grep -q ${appName}-${env}-chart ;then
+        if helm list -n ${namespace}| grep -q ${appName}-${env}-chart ;then
 
         echo "************** chat ${appName}-${env}-chart exists , proceeding with the chat upgarde **************"
         
